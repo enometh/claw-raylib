b09ea5e37e5d6b32534d9c007e96f2397c9b15df
Madhu <enometh@net.meer>
2024-01-15 17:15:56 +0530

    de-asdf: build with mk-defystem
    * config-madhu.lisp: define local config variables
    * cl-raylib.system: (claw-raylib/gen, claw-raylib/library): used here
    * library.lisp: used here
    * claw.lisp: used here.
    * claw-raylib-defsys.lisp: (claw-raylib-defsystem): define mk-defsystems
      system definitions.  claw-raylib.raylib, etc.
    * cl-raylib.system (claw-raylib/config-madhu, claw-raylib/defsys): new

---
+++ claw-raylib-defsys.lisp
@@ -0,0 +1,49 @@
+;;; -*- Mode: LISP; Package: :cl-user; BASE: 10; Syntax: ANSI-Common-Lisp; -*-
+;;;
+;;;   Time-stamp: <>
+;;;   Touched: Wed Oct 23 18:25:53 2024 +0530 <enometh@net.meer>
+;;;   Bugs-To: enometh@net.meer
+;;;   Status: Experimental.  Do not redistribute
+;;;   Copyright (C) 2024 Madhu.  All Rights Reserved.
+;;;
+(in-package "CL-USER")
+
+;;; claw-raylib-defsys.lisp. define mk-defsystem system definitions
+;;; for the generated bindings
+;;;
+;;; ;madhu 241023 split from claw.lisp
+
+(defmacro claw-raylib-defsystem (name)
+  (check-type name string)
+  (setq name (string-downcase name))
+  `(mk:defsystem ,(concatenate 'string "claw-raylib." (string-downcase name))
+     :source-pathname #.(pathname *claw-raylib-dir*)
+     :binary-pathname #.(merge-pathnames "fasl/" *claw-raylib-dir*)
+     :source-extension "lisp"
+     :depends-on ("uiop" "cffi"  "claw-cxx/util")
+     :components
+     ((:module ,name
+       :components ((:file "x86_64-pc-linux-gnu"
+		     :if-feature
+		     (:and :x86-64 :linux))
+		    #+nil
+		    (:file "i686-pc-linux-gnu"
+		     :if-feature (:and :x86 :linux))))
+      (:module "lib"
+       :components
+       ((:file ,(concatenate 'string "lib" name "-adapter.x86_64-pc-linux-gnu")
+	 :source-extension "c"
+	 :if-feature (:and :x86-64 :linux)
+	 :compile-only t ;; handle loading via library.lisp
+	 :language :claw-cxx-adapter
+	 :compiler-options
+	 (:cflags #.(format nil "-O0 -g3 -I~A" *claw-raylib-inc*)
+	  :ldflags #.(format nil "-Wl,-O1 -Wl,--as-needed -L~A" *claw-raylib-lib*)
+	  )))))))
+
+(progn
+(claw-raylib-defsystem "raylib")
+(claw-raylib-defsystem "rlgl")
+(claw-raylib-defsystem "raygui"))
+
+
--- library.lisp
+++ library.lisp
@@ -3,39 +3,54 @@
 
 (in-package #:claw-raylib.library)
 
+#+asdf
 (pushnew
  (namestring
-  (merge-pathnames #P"lib/" (asdf:component-pathname (asdf:find-system '#:claw-raylib))))
- cffi:*foreign-library-directories* :test #'string=)
+  (merge-pathnames #P"lib/"
+		   (asdf:component-pathname (asdf:find-system '#:claw-raylib))))
+ cffi:*foreign-library-directories* :test
+ ;; enometh/cffi extends the type of elements that can be found in
+ ;; cffi:*foreign-library-directories*
+ #'(lambda (x y)
+     (and (stringp x) (string y) (string= x y))))
+
+
+(eval-when (load eval compile)
+(defvar *dll-path*
+  (namestring
+   (merge-pathnames #P"fasl/lib/"
+		    cl-user::*claw-raylib-dir*))))
+
 
 (cffi:define-foreign-library libraylib
-  (:unix "libraylib.so")
+  (:unix "libraylib.so" :search-path #.cl-user::*claw-raylib-lib*)
   (t (:default "libraylib")))
 
 (cffi:use-foreign-library libraylib)
 
 (cffi:define-foreign-library libraylib-adapter
-  (:unix "libraylib-adapter.so")
+  (:unix "libraylib-adapter.x86_64-pc-linux-gnu.so" :search-path #.*dll-path*)
   (t (:default "libraylib-adapter")))
 
 (cffi:use-foreign-library libraylib-adapter)
 
-(push :raylib *features*)
+(pushnew :raylib *features*)
 
 (cffi:define-foreign-library librlgl-adapter
-  (:unix "librlgl-adapter.so")
+  (:unix "librlgl-adapter.x86_64-pc-linux-gnu.so" :search-path #.*dll-path*)
   (t (:default "librlgl-adapter")))
 
 (cffi:use-foreign-library librlgl-adapter)
 
-(push :rlgl *features*)
+(pushnew :rlgl *features*)
 
+;; not libraygui.so but raygui.so
 (cffi:define-foreign-library libraygui
-  (:unix "libraygui.so")
+  (:unix "raygui.so" :search-path #.cl-user::*claw-raylib-lib*)
   (t (:default "libraygui")))
 
 (cffi:define-foreign-library libraygui-adapter
-  (:unix "libraygui-adapter.so")
+  (:unix "libraygui-adapter.x86_64-pc-linux-gnu.so" :search-path #.*dll-path*)
   (t (:default "libraygui-adapter")))
 
 (handler-case

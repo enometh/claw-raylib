426ee13675b29655daf031be9a3e0f62ea466273
Madhu <enometh@net.meer>
2024-11-24 20:22:09 +0530

    NOTES-madhu.org, config-madhu.lisp: update targets to raylib-5.5

--- config-madhu.lisp
+++ config-madhu.lisp
@@ -6,5 +6,5 @@
 
 
 (defvar *claw-raylib-dir* "/dev/shm/claw-cxx-raylib/")
-(defvar *claw-raylib-inc* "/opt/raylib-5.0_linux_amd64/include")
-(defvar *claw-raylib-lib* "/opt/raylib-5.0_linux_amd64/lib/")
+(defvar *claw-raylib-inc* "/opt/raylib-5.5_linux_amd64/include")
+(defvar *claw-raylib-lib* "/opt/raylib-5.5_linux_amd64/lib/")
